fa09cc0bc0ab4fdfff8ad5d507a90159587cdba1
mcglincy <mattmcglincy@gmail.com>
2022-05-25 15:00:55 -0500

    Work on item sheets.

--- module/actor/character-sheet.js
+++ module/actor/character-sheet.js
@@ -7,14 +7,15 @@ export class CYCharacterSheet extends ActorSheet {
       classes: ["cy", "sheet", "actor", "character"],
       template: "systems/cy_borg/templates/actor/character-sheet.html",
       width: 402,
-      height: 844,
+      height: 900,
       tabs: [
         {
           navSelector: ".sheet-tabs",
           contentSelector: ".sheet-body",
-          initial: "notes",
+          initial: "combat",
         },
       ],
+      // TODO: decide dragDrop setup
       dragDrop: [{ dragSelector: ".item-list .item", dropSelector: null }],
     });
   }
@@ -46,6 +47,35 @@ export class CYCharacterSheet extends ActorSheet {
     superData.data.data.weapons = superData.data.items
       .filter((item) => item.type === CONFIG.CY.itemTypes.weapon)
       .sort(byName);
+
+    console.log(superData);
     return superData;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+    // html.find(".item-create").click(this._onItemCreate.bind(this));
+    html.find(".item-edit").click(this._onItemEdit.bind(this));
+    html.find(".item-delete").click(this._onItemDelete.bind(this));
+    // html.find(".inline-edit").change(this._onInlineEdit.bind(this));
+    // html.find(".ability-name").click(this._onAbilityRoll.bind(this));
+    // html.find("a.regenerate").click(this._onRegenerate.bind(this));
+  }  
+  
+  _onItemEdit(event) {
+    const row = $(event.currentTarget).parents(".item");
+    if (row) {
+      const item = this.actor.items.get(row.data("itemId"));
+      if (item) {
+        item.sheet.render(true);
+      }
+    }
+  }
+
+  _onItemDelete(event) {
+    const row = $(event.currentTarget).parents(".item");
+    this.actor.deleteEmbeddedDocuments("Item", [row.data("itemId")]);
+    row.slideUp(200, () => this.render(false));
   }  
  }
--- module/cy_borg.js
+++ module/cy_borg.js
@@ -1,6 +1,7 @@
 
 import { CY } from "./config.js";
 import { CYCharacterSheet } from "./actor/character-sheet.js";
+import { CYItemSheet } from "./item/item-sheet.js";
 
 Hooks.once("init", async function () {
   consoleBanner();
@@ -29,13 +30,23 @@ const registerHandlebarsPartials = async () => {
     "systems/cy_borg/templates/actor/equipment-tab.html",
     "systems/cy_borg/templates/actor/feats-tab.html",
     "systems/cy_borg/templates/actor/nano-tab.html",
+    "systems/cy_borg/templates/item/item-base-fields.html",
+    "systems/cy_borg/templates/item/item-description-tab.html",
+    "systems/cy_borg/templates/item/item-sheet-header.html",
+    "systems/cy_borg/templates/item/item-sheet-tabs.html",
   ]);
 }
+
 const registerSheets = () => {
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet(CY.system, CYCharacterSheet, {
     types: ["character"],
     makeDefault: true,
-    label: "CY.CharacterSheetClass",
+    label: "CY.CharacterSheet",
+  });  
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet(CY.system, CYItemSheet, {
+    makeDefault: true,
+    label: "CY.ItemSheet",
   });  
 };
