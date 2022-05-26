548884f7b0fd042525fc183bed04e6db163c188c
mcglincy <mattmcglincy@gmail.com>
2022-05-25 20:25:54 -0500

    Iterating.

---
+++ module/item/item-sheet.js
@@ -0,0 +1,23 @@
+export class CYItemSheet extends ItemSheet {
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["cy", "sheet", "item"],
+      width: 402,
+      height: 900,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "details",
+        },
+      ],
+    });
+  }
+
+  /** @inheritdoc */
+  get template() {
+    const path = "systems/cy_borg/templates/item/";
+    return `${path}/${this.item.data.type}-sheet.html`;
+  }  
+}
