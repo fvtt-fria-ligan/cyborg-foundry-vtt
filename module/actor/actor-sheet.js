e636b84b6b5d1901ef227bdc96d2b6064be26898
mcglincy <mattmcglincy@gmail.com>
2022-06-14 20:30:03 -0500

    Add +/- quantity buttons.

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -10,6 +10,8 @@ import { byName } from "../utils.js";
     super.activateListeners(html);
     html.find(".item-edit").click(this._onItemEdit.bind(this));
     html.find(".item-delete").click(this._onItemDelete.bind(this));
+    html.find(".item-qty-plus").click(this._onItemAddQuantity.bind(this));
+    html.find(".item-qty-minus").click(this._onItemSubtractQuantity.bind(this));
     html.find(".add-item-button").on("click", this._addItem.bind(this));
   }  
   
@@ -48,4 +50,35 @@ import { byName } from "../utils.js";
     event.preventDefault();
     showAddItemDialog(this.actor);
   }
+
+  /**
+   * Handle adding quantity of an Owned Item within the Actor
+   */
+   async _onItemAddQuantity(event) {
+    event.preventDefault();
+    const anchor = $(event.currentTarget);
+    const li = anchor.parents(".item");
+    const itemId = li.data("itemId");
+    const item = this.actor.items.get(itemId);
+    const attr = "data.quantity";
+    const currQuantity = getProperty(item.data, attr);
+    return item.update({ [attr]: currQuantity + 1 });
+  }
+
+  /**
+   * Handle subtracting quantity of an Owned Item within the Actor
+   */
+  async _onItemSubtractQuantity(event) {
+    event.preventDefault();
+    const anchor = $(event.currentTarget);
+    const li = anchor.parents(".item");
+    const itemId = li.data("itemId");
+    const item = this.actor.items.get(itemId);
+    const attr = "data.quantity";
+    const currQuantity = getProperty(item.data, attr);
+    // can't reduce quantity below one
+    if (currQuantity > 1) {
+      return item.update({ [attr]: currQuantity - 1 });
+    }
+  }  
  }
--- module/ads/ad-spammer.js
+++ module/ads/ad-spammer.js
@@ -1,4 +1,9 @@
 
-export const showAd = async () => {
-
+export const showChatAd = async () => {
+  //const html = await renderTemplate(ATTACK_ROLL_CARD_TEMPLATE, rollResult);
+  const html = "I am an add";
+  ChatMessage.create({
+    content: html,
+    // speaker: ChatMessage.getSpeaker({ actor }),
+  });
 }
