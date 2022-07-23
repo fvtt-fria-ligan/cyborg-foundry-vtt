b0dcd5992808ec2ed35f12b3aea9ce738bf38ce8
mcglincy <mattmcglincy@gmail.com>
2022-07-23 13:58:42 -0500

    Start fixing for v10.

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -38,13 +38,14 @@ export class CYItemSheet extends ItemSheet {
   /** @inheritdoc */
   get template() {
     const path = "systems/cy_borg/templates/item/";
-    return `${path}/${this.item.data.type}-sheet.html`;
+    return `${path}/${this.item.type}-sheet.html`;
   }
 
   /** @override */
   getData() {
     const superData = super.getData();
-    superData.cssCyberClass = superData.data.data.cybertech ? "cyber": "";
+    superData.cssCyberClass = superData.data.system.cybertech ? "cyber" : "";
+    console.log(superData);
     return superData;
   }
 
