8a2f94762ab8718a2473ddc306d098a2620f5338
mcglincy <mattmcglincy@gmail.com>
2022-08-10 15:19:29 -0500

    Tweak actor sheet headers, and override token button label for our thin sheet.

--- module/actor/foe-sheet.js
+++ module/actor/foe-sheet.js
@@ -8,7 +8,7 @@ export class CYFoeSheet extends CYActorSheet {
     return mergeObject(super.defaultOptions, {
       classes: ["cy", "sheet", "actor", "foe"],
       template: "systems/cy_borg/templates/actor/foe-sheet.html",
-      width: 402,
+      width: 406,
       height: 900,
       tabs: [
         {
