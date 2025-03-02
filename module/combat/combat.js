9ca5f609150d1d0ca339a2018d8ea2296d177431
Ian Guswiler <iangus97@gmail.com>
2025-03-02 15:05:07 -0500

    remove unused code

--- module/combat/combat.js
+++ module/combat/combat.js
@@ -67,38 +67,4 @@ export class CYCombat extends Combat {
 
     return this;
   }
-
-  // /**
-  //  * Define how the array of Combatants is sorted in the displayed list of the tracker.
-  //  * This method can be overridden by a system or module which needs to display combatants in an alternative order.
-  //  * By default sort by initiative, falling back to name
-  //  * @private
-  //  */
-  // _sortCombatants(a, b) {
-  //   // .combat is a getter, so verify existence of combats array
-  //   if (game.combats && game.combat?.partyInitiative) {
-  //     const isPartyA = game.combat.isFriendlyCombatant(a);
-  //     const isPartyB = game.combat.isFriendlyCombatant(b);
-  //     if (isPartyA !== isPartyB) {
-  //       // only matters if they're different
-  //       if (game.combat.partyInitiative > 3) {
-  //         // players begin
-  //         return isPartyA ? -1 : 1;
-  //       } else {
-  //         // enemies begin
-  //         return isPartyA ? 1 : -1;
-  //       }
-  //     }
-  //   }
-
-  //   // combatants are both friendly or enemy, so sort by normal initiative
-  //   const ia = Number.isNumeric(a.initiative) ? a.initiative : -9999;
-  //   const ib = Number.isNumeric(b.initiative) ? b.initiative : -9999;
-  //   const ci = ib - ia;
-  //   if (ci !== 0) return ci;
-  //   const [an, bn] = [a.token?.name || "", b.token?.name || ""];
-  //   const cn = an.localeCompare(bn);
-  //   if (cn !== 0) return cn;
-  //   return a.tokenId - b.tokenId;
-  // }
 }
