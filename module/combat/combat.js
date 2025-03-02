510b5ace3545c4007dc4317d51253dc5a327ba0e
Ian Guswiler <iangus97@gmail.com>
2025-03-02 14:26:37 -0500

    refactor to set 1/0 initiative values based on party roll

--- module/combat/combat.js
+++ module/combat/combat.js
@@ -4,19 +4,17 @@ export class CYCombat extends Combat {
     await game.combat.resortCombatants();
   }
 
-  async resortCombatants() {
-    // TODO: this seems like a stupidly-hacky way to do this. Is there no better way?
+  async setCombatantsInitiative() {
     const updates = this.turns.map((t) => {
       return {
         _id: t.id,
-        initiative: t.system.initiative,
+        initiative: this.#getInitiative(t),
       };
     });
-    await game.combat.resetAll();
     await this.updateEmbeddedDocuments("Combatant", updates);
   }
 
-  isFriendlyCombatant(combatant) {
+  #isFriendlyCombatant(combatant) {
     if (combatant._token) {
       // v8 compatible
       return combatant._token.system.disposition === 1;
@@ -30,37 +28,49 @@ export class CYCombat extends Combat {
     return false;
   }
 
-  /**
-   * Define how the array of Combatants is sorted in the displayed list of the tracker.
-   * This method can be overridden by a system or module which needs to display combatants in an alternative order.
-   * By default sort by initiative, falling back to name
-   * @private
-   */
-  _sortCombatants(a, b) {
-    // .combat is a getter, so verify existence of combats array
-    if (game.combats && game.combat.partyInitiative) {
-      const isPartyA = game.combat.isFriendlyCombatant(a);
-      const isPartyB = game.combat.isFriendlyCombatant(b);
-      if (isPartyA !== isPartyB) {
-        // only matters if they're different
-        if (game.combat.partyInitiative > 3) {
-          // players begin
-          return isPartyA ? -1 : 1;
-        } else {
-          // enemies begin
-          return isPartyA ? 1 : -1;
-        }
-      }
+  #getInitiative(combatant) {
+    if (this.partyInitiative == null) {
+      return null;
     }
 
-    // combatants are both friendly or enemy, so sort by normal initiative
-    const ia = Number.isNumeric(a.initiative) ? a.initiative : -9999;
-    const ib = Number.isNumeric(b.initiative) ? b.initiative : -9999;
-    const ci = ib - ia;
-    if (ci !== 0) return ci;
-    const [an, bn] = [a.token?.name || "", b.token?.name || ""];
-    const cn = an.localeCompare(bn);
-    if (cn !== 0) return cn;
-    return a.tokenId - b.tokenId;
+    if (this.#isFriendlyCombatant(combatant)) {
+      return this.partyInitiative >= 4;
+    } else {
+      return this.partyInitiative <= 3;
+    }
   }
+
+  // /**
+  //  * Define how the array of Combatants is sorted in the displayed list of the tracker.
+  //  * This method can be overridden by a system or module which needs to display combatants in an alternative order.
+  //  * By default sort by initiative, falling back to name
+  //  * @private
+  //  */
+  // _sortCombatants(a, b) {
+  //   // .combat is a getter, so verify existence of combats array
+  //   if (game.combats && game.combat?.partyInitiative) {
+  //     const isPartyA = game.combat.isFriendlyCombatant(a);
+  //     const isPartyB = game.combat.isFriendlyCombatant(b);
+  //     if (isPartyA !== isPartyB) {
+  //       // only matters if they're different
+  //       if (game.combat.partyInitiative > 3) {
+  //         // players begin
+  //         return isPartyA ? -1 : 1;
+  //       } else {
+  //         // enemies begin
+  //         return isPartyA ? 1 : -1;
+  //       }
+  //     }
+  //   }
+
+  //   // combatants are both friendly or enemy, so sort by normal initiative
+  //   const ia = Number.isNumeric(a.initiative) ? a.initiative : -9999;
+  //   const ib = Number.isNumeric(b.initiative) ? b.initiative : -9999;
+  //   const ci = ib - ia;
+  //   if (ci !== 0) return ci;
+  //   const [an, bn] = [a.token?.name || "", b.token?.name || ""];
+  //   const cn = an.localeCompare(bn);
+  //   if (cn !== 0) return cn;
+  //   return a.tokenId - b.tokenId;
+  // }
 }
